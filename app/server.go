00cfa6d06835095d5a21d171bfca5ecf5aecf46d
Remus Poienar <11542010+remuspoienar@users.noreply.github.com>
2024-03-31 14:23:05 +0300

    Replication stage 1

--- app/server.go
+++ app/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 	"github.com/codecrafters-io/redis-starter-go/app/storage"
@@ -12,13 +13,13 @@ import (
 )
 
 const (
-	PORT = "6379"
-	PING = "PING"
-	PONG = "PONG"
-	DOCS = "COMMAND DOCS"
-	ECHO = "ECHO"
-	GET  = "GET"
-	SET  = "SET"
+	DefaultPort = 6379
+	PING        = "PING"
+	PONG        = "PONG"
+	DOCS        = "COMMAND DOCS"
+	ECHO        = "ECHO"
+	GET         = "GET"
+	SET         = "SET"
 )
 
 func closeConnections(closable any) {
@@ -35,16 +36,20 @@ func closeConnections(closable any) {
 }
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", DefaultPort, "Port to run server(positive integer)")
+	flag.Parse()
 	db := storage.NewDb()
-	l, err := net.Listen("tcp", "0.0.0.0:"+PORT)
+	address := fmt.Sprintf("0.0.0.0:%d", port)
+	l, err := net.Listen("tcp", address)
 
 	if err != nil {
-		fmt.Println("Failed to bind to port " + PORT)
+		fmt.Println("Failed to bind port on " + address)
 		os.Exit(1)
 	}
 	defer closeConnections(l)
 
-	fmt.Println("Server is listening on port " + PORT)
+	fmt.Println("Server is listening on " + address)
 
 	for {
 		conn, err := l.Accept()
